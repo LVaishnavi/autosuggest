bed9656ded0a8749523273d7b5ea5fb09ad7cc6c
ch Lakshmi Vaishnavi <24wh1a05a9@bvrithyderabad.edu.in.com>
2026-06-19 20:19:35 +0530

    created toggle page and started creating random-user page

---
+++ Frontend/js/toggle.js
@@ -0,0 +1,29 @@
+const img = document.querySelector('.card img');
+const title = document.querySelector('.card h2');
+const genderText = document.querySelector('.card p');
+const toggleButton = document.getElementById('toggleGenderBtn');
+
+const maleState = {
+    imgSrc: '../images/john.png',
+    name: 'John Doe',
+    gender: 'Male',
+    alt: 'John Doe'
+};
+
+const femaleState = {
+    imgSrc: '../images/john-female.png',
+    name: 'John Female',
+    gender: 'Female',
+    alt: 'John Female'
+};
+
+let isMale = true;
+
+toggleButton.addEventListener('click', () => {
+    const state = isMale ? femaleState : maleState;
+    img.src = state.imgSrc;
+    img.alt = state.alt; 
+    title.textContent = state.name;
+    genderText.textContent = state.gender;
+    isMale = !isMale;
+});
