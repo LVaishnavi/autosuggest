e2fe8fcecfdfd48ab294ed67c950a811c0a09306
ch Lakshmi Vaishnavi <24wh1a05a9@bvrithyderabad.edu.in.com>
2026-06-19 12:22:39 +0530

    javascript code

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 6700;
+
+// to tell app(or web server), where static files -html/css/js are stored
+app.use(express.static("Frontend"));
+
+app.listen(port, function(){
+    console.log("App running on http://localhost:" + port);
+});
+
