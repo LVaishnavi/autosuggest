3cd6dfaae8c40be074c52bc09a36908be0a42563
ch Lakshmi Vaishnavi <24wh1a05a9@bvrithyderabad.edu.in.com>
2026-06-20 10:39:38 +0530

    created random user page

---
+++ Frontend/js/random-user.js
@@ -0,0 +1,26 @@
+function randomuser(){
+    fetch('https://randomuser.me/api/')
+    .then(function(res){
+        return res.json();
+
+    })
+    .then(function(data){
+        var user = data.results[0];
+        var fullname = user.name.title + " " + user.name.first + " " + user.name.last;
+        
+            document.getElementById("user-img").src = user.picture.large;
+            
+            // update the name and gender
+            //name
+            document.getElementById("user-name").innerText = fullname;
+    
+            //gender
+            document.getElementById("user-gender").innerText = user.gender;
+
+    })
+    .catch(function(err){
+        console.log("Error: "+err);
+    })
+}
+
+
